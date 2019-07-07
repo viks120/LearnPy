a25092807667cd71430eeb711ff0400682b17522
Vikrant <viks120@users.noreply.github.com>
2019-07-07 16:19:58 -0700

    vid-3

--- pyblog/django_project/blog/views.py
+++ pyblog/django_project/blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author':'Viks',
+        'title':'post 1',
+        'content':'content 1',
+        'date_posted':'August 27, 2018'
+    },
+    {
+        'author':'James Bond',
+        'title':'post 2',
+        'content':'content 2',
+        'date_posted':'August 28, 2018'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title':'about'})
